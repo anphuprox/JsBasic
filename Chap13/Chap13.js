fc3fa5de34f1ee5d59572b98e98736c54939eb03
anphuprox <75016328+anphuprox@users.noreply.github.com>
2024-05-29 21:19:27 +0700

    chap13

---
+++ Chap13/Chap13.js
@@ -0,0 +1,25 @@
+let arr=['spur','mu','liver']
+var i=0
+while(i<arr.length){
+    if(arr[i].length<=2){
+        console.log(arr[i])
+    }
+    else if (arr[i].length===5)
+    {
+        console.log(arr[i]);
+    }
+    i++;
+}
+console.log('-------------------')
+var j=0
+
+while (j<arr.length){
+    if (arr[j]==='spur'){
+        console.log('loser');
+        console.log(j);
+        break;
+    }
+    j++;
+}
+console.log('-------------------')
+
