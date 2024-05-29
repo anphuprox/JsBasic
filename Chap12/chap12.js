8fac79831045a72d0225373cb28970f5374dd1f5
anphuprox <75016328+anphuprox@users.noreply.github.com>
2024-05-29 20:46:19 +0700

    chap12

---
+++ Chap12/chap12.js
@@ -0,0 +1,19 @@
+
+
+var i=0
+let arr=['real','mu','spur']
+
+while(i<arr.length){
+    console.log(arr[i]);
+    i++;
+}
+let j=0
+let flag=false
+
+while (!flag){
+    console.log(j)
+j++;
+    if(j===10){
+        flag=true;
+    }
+}
