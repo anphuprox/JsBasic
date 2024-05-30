6fd87ed9012e9cde5743aadcbfca73da193cfb17
anphuprox <75016328+anphuprox@users.noreply.github.com>
2024-05-30 15:42:29 +0700

    chap17

---
+++ Chap17/Chap17.js
@@ -0,0 +1,15 @@
+console.log('dddd')
+
+function testfuc(x,y,i){
+
+console.log(1);
+console.log(2);
+console.log(3);
+if(i===5)return;
+console.log(4);
+console.log(5);
+return x+y
+
+
+}
+console.log(`x+y`,testfuc(4,5,5))
