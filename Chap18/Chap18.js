6445d9b7cade777093cd44cb540d5f4be35e6720
anphuprox <75016328+anphuprox@users.noreply.github.com>
2024-05-30 20:28:56 +0700

    chap18,19

---
+++ Chap18/Chap18.js
@@ -0,0 +1,4 @@
+let sum=(a,b)=>{
+    return a+b
+}
+console.log(`sum:`,sum(6,9))
---
+++ Chap19/Chap19.js
@@ -0,0 +1,8 @@
+let obj ={
+    name:'annfus',
+    address:'real',
+    getname:function(){
+        return this.name;
+    }
+}
+    console.log('nameobj',obj.getname())
